da024da7b1c266a9b5ce1ec136344fbbde32edb6
moyoonthego <39111972+moyoonthego@users.noreply.github.com>
2019-03-11 03:50:20 -0400

    Initialized needed docs

---
+++ Main.v
@@ -0,0 +1,13 @@
+module main (SW, KEY);  // keyboard selection keys are missing + sounddevices output
+	output KEY[3:0];    // KEY0 is our record button. KEY0 again is stop recording. KEY3 is total reset (of saved recordings).
+	output SW[2:0];    // SW2,1,0 are our registers for saved recordings
+	
+	// simply call the music player module:
+	
+	
+	// simply call the recording saver module:
+	
+	
+	// simply call the recording player module:
+	
+endmodule
