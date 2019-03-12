966738d7819fe1eb51e269d87b062441a4f2a26b
moyoonthego <39111972+moyoonthego@users.noreply.github.com>
2019-03-12 15:53:58 -0400

    Added visual display

---
+++ Modules/BoardDisplay.v
@@ -0,0 +1,86 @@
+module show_key (ascii_val, note, symbol);
+	input [6:0] ascii_val;
+	output [7:0] symbol;
+	output [7:0] note;
+
+	// drawing key based on the key that was clicked
+	always @(*)
+	begin: display_notes
+		// default case -> don't display anything
+		note = 7'b1111111;
+		symbol = 7'b1111111;
+		// @ROBERT -- YOU CAN EDIT THESE DISPLAYS BASED ON WHAT YOU THINK THE NOTES SHOULD BE
+		case(ascii_val)
+			7'd81: begin // This note is: C-
+				   note = 7'b1000110;
+				   symbol = 7'b0111111;
+				   end
+			7'd87: begin // This note is: C
+				   note = 7'b1000110;
+				   end
+			7'd69: begin // This note is: C sharp
+				   note = 7'b1000110;
+				   symbol = 7'b0000011;
+				   end
+			7'd82: begin // This note is: D-
+				   note = 7'b1000000;
+				   symbol = 7'b0111111;
+				   end
+			7'd84: begin // This note is: D
+				   note = 7'b1000000;
+				   end
+			7'd89: begin // This note is: D sharp
+				   note = 7'b1000000;
+				   symbol = 7'b0000011;
+				   end
+			7'd85: begin // This note is: E-
+				   note = 7'b0000110;
+				   symbol = 7'b0111111;
+				   end
+			7'd73: begin // This note is: E
+				   note = 7'b0000110;
+				   end
+			7'd79: begin // This note is: E sharp
+				   note = 7'b0000110;
+				   symbol = 7'b0000011;
+				   end
+			7'd80: begin // This note is: F-
+				   note = 7'b0001110;
+				   symbol = 7'b0111111;
+				   end
+			7'd65: begin // This note is: F
+				   note = 7'b0001110;
+				   end
+			7'd83: begin // This note is: F sharp
+				   note = 7'b0001110;
+				   symbol = 7'b0000011;
+				   end
+			7'd68: begin // This note is: A-
+				   note = 7'b0001000;
+				   symbol = 7'b0111111;
+				   end
+			7'd70: begin // This note is: A
+				   note = 7'b0001000;
+				   end
+			7'd71: begin // This note is: A sharp
+				   note = 7'b0001000;
+				   symbol = 7'b0000011;
+				   end
+			7'd72: begin // This note is: B-
+				   note = 7'b0000000;
+				   symbol = 7'b0111111;
+				   end
+			7'd74: begin // This note is: B
+				   note = 7'b0000000;
+				   end
+			7'd75: begin // This note is: B sharp
+				   note = 7'b0000000;
+				   symbol = 7'b0000011;
+				   end
+			7'd76: begin // This note is: T (extra)
+				   note = 7'b1111000;
+				   end
+		endcase
+
+	end	
+endmodule
--- Modules/Main.v
+++ Modules/Main.v
@@ -1,13 +1,18 @@
-module main (SW, KEY, PS2_KBCLK, PS2_KBDAT, GPIO);
+module main (SW, KEY, PS2_KBCLK, PS2_KBDAT, GPIO, HEX0, HEX1);
 	input [3:0] KEY;     // KEY0 is our record button. KEY0 again is stop recording (saved recordings can be written over, so no reset button needed).
 	input [2:0] SW;      // SW2,1,0 are our registers for saved recordings
 	input PS2_KBCLK;    // Clock input from the keyboard
 	input PS2_KBDAT;    // Data input from the keyboard (byte)      (This and the above input need to be deciphered to values we can work with)
 	output [35:0] GPIO; // These are our buzzer speakers. 7-0 is main buzzer. 35-28 is register buzzer #1. 27-20 is #2. 19-12 is #3.
 	
+	output [7:0] HEX0; // NOT NEEDED - DESIGN
+	output [7:0] HEX1; // ^
+	
 	// Convert the keyboard input into processable data (ie, get ascii symbol of key)
 	wire [6:0] ascii_val;
 	read_keyboard ps2(.kb_data(PS2_KBDAT), .kb_clock(PS2_KBCLK), .ascii(ascii_val[6:0]));
+	
+	show_key keys(ascii_val[6:0], HEX1[7:0], HEX0[7:0]); // visual display -> showing the keys being used (for testing and looking pretty lol)
 	
 	// Simply call the music player module:
 	play_music buzzer1(.key(ascii_val[6:0]), .buzzer(GPIO[7:0])); // NOT DONE, MORE I/O present
