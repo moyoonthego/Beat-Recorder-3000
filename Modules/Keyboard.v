f0ef50ecc6477a2939518a51b3bcd20b7b76212f
moyoonthego <39111972+moyoonthego@users.noreply.github.com>
2019-03-11 21:00:31 -0400

    Update Keyboard.v

--- Modules/Keyboard.v
+++ Modules/Keyboard.v
@@ -4,15 +4,72 @@ module read_keyboard (kb_data, kb_clock, ascii);
 	output [6:0] ascii;
 	wire [7:0] raw_data;
 	
+	//Convert keyboard inputs to raw data
+	get_raw keys(kb_clock, kb_data, raw_data[7:0]);
+	
 	// Last step: process ascii's
-	convert_readable(raw_data[7:0], ascii[6:0]);
+	convert_readable chars(raw_data[7:0], ascii[6:0]);
 	
 endmodule
 
-module convert_readable(
-	input [7:0] raw_data,
-	output reg [6:0] ascii
-);
+
+module get_raw(clk, data, raw);
+    input wire clk; // Clock pin form keyboard
+    input wire data; //Data pin form keyboard
+    output reg [7:0] raw;
+
+
+    reg [7:0] data_curr;
+    reg [7:0] data_pre;
+    reg [3:0] b;
+    reg flag;
+    
+    initial
+    begin
+        b<=4'h1;
+        flag<=1'b0;
+        data_curr<=8'hf0;
+        data_pre<=8'hf0;
+        raw<=8'hf0;
+    end
+        
+    always @(negedge clk) //Activating at negative edge of clock from keyboard
+    begin
+    
+    case(b)
+        1:; //first bit
+        2:data_curr[0]<=data;
+        3:data_curr[1]<=data;
+        4:data_curr[2]<=data;
+        5:data_curr[3]<=data;
+        6:data_curr[4]<=data;
+        7:data_curr[5]<=data;
+        8:data_curr[6]<=data;
+        9:data_curr[7]<=data;
+        10:flag<=1'b1; //Parity bit
+        11:flag<=1'b0; //Ending bit
+    endcase
+    
+    if(b<=10)
+        b<=b+1;
+    else if(b==11)
+        b<=1;
+    end
+    
+    always@(posedge flag) // Printing data obtained to led
+    begin
+    if(data_curr==8'hf0)
+        raw<=data_pre;
+    else
+        data_pre<=data_curr;
+    end
+endmodule
+
+
+
+module convert_readable(raw_data, ascii);
+    input [7:0] raw_data;
+	output reg [6:0] ascii;
 	
 	always @(*)
 	begin
